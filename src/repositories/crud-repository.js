25f3e3bff2f230a5c015ab8593cdc6cc02935638
athreya-harshith <athreya1207@gmail.com>
2023-09-10 08:26:40 +0530

    Use this commit to get started with the CRUD operations with crud-repository.js (Updated One) To get this particular version run the command ``` git checkout <commit-ID> ``` The #<commit-ID> can be found in the commits in github After Getting back to this version, Run the following commands to complete the setup ```npm install``` It installs all the mentioned packages in package.json file After this, go to src folder and run ```npx sequelize init --force`` This creates config.json file inside config folder , creates models, migrations and seeders Now after this setup , Create a .env file in root directory and make a PORT=<value> to specify the port Good to go !!!!!!!!! Now in root folder run ```node --watch src/index.js ``` this starts the server. To check if the Server is live make a get request to localhost:<port-value>/api/v1/info

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -1,4 +1,6 @@
+const { StatusCodes } = require('http-status-codes');
 const {Logger} = require('../config');
+const AppError = require('../utils/errors/app-error');
 class CrudRepository
 {
     constructor(model)
@@ -21,72 +23,54 @@ class CrudRepository
         // before it was not commented 
         
         // after commenting , added this : ->> if this throws an error its sent to services
+        console.log(data);
         const response = await this.model.create(data);
         return response;
     }
 
     async destroy(data)
     {
-        try
-        {
-            const response = await this.model.destroy({
-                where:{
-                    id:data
-                }
-            });
-            return response;
-        }
-        catch(error)
-        {
-            Logger.error('Something went wrong in the crud repo : destroy');
-            throw error;
-        }
+        const response = await this.model.destroy({
+            where:{
+                id:data
+            }
+        });
+        if(!response)
+            {
+                throw new AppError('The Resource not found to delete',StatusCodes.NOT_FOUND);
+            }
+        return response;
     }
 
     async get(data)
     {
-        try
+        const response = await this.model.findByPk(data);
+        if(!response)
         {
-            const response = await this.model.findByPk(data);
-            return response;
+            // handling this error in crud-repository is better in general it will handle automatically
+            throw new AppError('Requested Resource Not Found to fetch',StatusCodes.NOT_FOUND);
         }
-        catch(error)
-        {
-            Logger.error('Something went wrong in the crud repo : get');
-            throw error;
-        }
+        return response;
     }
 
     async getAll()
     {
-        try
-        {
-            const response = await this.model.findAll();
-            return response;
-        }
-        catch(error)
-        {
-            Logger.error('Something went wrong in the crud repo : getAll');
-            throw error;
-        }
+        const response = await this.model.findAll();
+        return response;
     }
 
     async update(id,data)// data is object {col:value,col:value,....}
     {
-        try
-        {
-            const response = await this.model.update(data,{
-                where:{
-                    id:id
-                }
-            })
-            return response;
-        }
-        catch(error)
-        {
-            Logger.error('Something went wrong in the crud repo : update');
-            throw error;
-        }
+        // console.log('running update method',id,'\n',data);
+        const response = await this.model.update(data,{
+            where:{
+                id:id
+            }
+        });
+        if(!(response[0]))// because it gives the data as an array 
+            throw new AppError('Requested Resource not found to update',StatusCodes.NOT_FOUND);
+        return response;
+        
     }
 }
 
--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,3 +1,2 @@
 module.exports = {
-    AirplaneRepository : require('./airplane-repository')
 };
